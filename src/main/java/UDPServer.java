12faca7291044bc9f3ecd61ca4410b066a274601
imabhijit <abhijit.gupta1122@gmail.com>
2020-04-04 21:31:25 -0400

    worked on server side

--- src/main/java/UDPServer.java
+++ src/main/java/UDPServer.java
@@ -1,17 +1,23 @@
+import ClientFromA1.RequestType;
+import ServerFromA2.Status;
 import joptsimple.OptionParser;
 import joptsimple.OptionSet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.channels.DatagramChannel;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
 
-import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.util.Arrays.asList;
 
 public class UDPServer {
@@ -22,15 +28,27 @@ public class UDPServer {
     private static final int SYN_ACK = 2;
     private static final int ACK = 3;
     private static final int NAK = 4;
+    private static final int FIN = 5;
 
     private static int windowHead = 0;
     private static int windowEnd = 0;
     private static int numberOfPackets = 0;
     private static long sequenceNumber = 0;
+    private static ArrayList<String> payloadList;
+
+    private static String httpVersion;
+    private static String filePath;
+    private static String pathToMainDirectory = "java/documents";
+    private static String requestSpecification = "";
+    private static boolean debugging;
+    private static String headers = "";
+    private static String data = "";
+    private static RequestType requestType;
 
     private void listenAndServe(int port) throws IOException {
 
         try (DatagramChannel channel = DatagramChannel.open()) {
+            payloadList = new ArrayList<>();
             channel.bind(new InetSocketAddress(port));
             logger.info("EchoServer is listening at {}", channel.getLocalAddress());
             ByteBuffer buf = ByteBuffer
@@ -44,7 +62,8 @@ public class UDPServer {
                 int responseType = 0;
                 Packet receivedPacket = receivePacket(channel, buf, router);
                 String payload = new String(receivedPacket.getPayload(), StandardCharsets.UTF_8);
-                switch (receivedPacket.getType()){
+                int requestType = receivedPacket.getType();
+                switch (requestType){
                     case SYN:
                         responseType = SYN_ACK;
                         payload = "SYN_ACK";
@@ -54,6 +73,10 @@ public class UDPServer {
                         break;
                     case DATA:
                         responseType = ACK;
+                        payloadList.set((int) receivedPacket.getSequenceNumber(), payload);
+                        break;
+                    case FIN:
+                        serveResource();
                         break;
                     default:
                         responseType = NAK;
@@ -63,13 +86,133 @@ public class UDPServer {
                 // The peer address of the packet is the address of the client already.
                 // We can use toBuilder to copy properties of the current packet.
                 // This demonstrate how to create a new packet from an existing packet.
-                if(receivedPacket.getType() != ACK) {
+                if(requestType != ACK && requestType != FIN) {
                     Packet resp = makeResponsePacket(responseType, payload, receivedPacket);
                     channel.send(resp.toBuffer(), router);
                 }
-
             }
         }
+    }
+
+    private void serveResource() {
+        String requestedData = packetPayloadsToString();
+        String resourceData = getResource(requestedData);
+        //TODO: break into packets and send it to client
+    }
+
+    private String packetPayloadsToString() {
+        StringBuilder sb = new StringBuilder();
+        for(String payload: payloadList){
+            sb.append(payload);
+        }
+        return sb.toString();
+    }
+
+    private String getResource(String requestedData) {
+        StringBuilder responseWriter = null;
+        String resource = "";
+        try {
+            BufferedReader requestReader = new BufferedReader(new StringReader(requestedData));
+            responseWriter = new StringBuilder();
+            String request = parseRequest(requestReader);
+            resource = createResponse(request);
+            System.out.println(resource);
+
+            resetVars();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e){
+            e.printStackTrace();
+            System.out.println("Something went wrong, try again");
+            responseWriter.append(requestSpecification + httpVersion + " " + Status.BAD_REQUEST.toString() + "\r\n" + headers + "\r\n");
+        }
+        return resource;
+    }
+
+    private static String parseRequest(BufferedReader requestReader) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        String line = requestReader.readLine();
+        requestType = line.split(" ")[0].equalsIgnoreCase("GET") ? RequestType.GET : RequestType.POST;
+        filePath = line.split(" ")[1];
+        httpVersion = line.split(" ")[2];
+        int count = 0;
+        while (line != null) {
+            sb.append(line + "\n");
+            line = requestReader.readLine();
+
+            if (line.isEmpty()) break;
+
+            if (line.contains("Content-Length")) {
+                count = Integer.valueOf(line.split(":")[1]);
+            }
+
+            if (!line.contains("Content-")) {
+                headers = headers + line + "\r\n";
+            }
+        }
+
+        for (int i = 0; i < count; i++) {
+            data = data + (char) requestReader.read();
+        }
+
+        requestSpecification = (debugging) ? sb.toString() + "\r\n" + data + "\r\n" : "";
+        return sb.toString();
+    }
+
+    public static String createResponse(String requestString) {
+        if (requestType == RequestType.GET) {
+            return getResponse();
+        } else if (requestType == RequestType.POST) {
+            return postResponse(requestString);
+        } else
+            return requestSpecification + httpVersion + " " + Status.BAD_REQUEST.toString() + "\r\n" + headers + "\r\n";
+    }
+
+    public static String postResponse(String requestString) {
+        String status = Status.OK.toString();
+        String contentType = "text/html";
+        if (filePath.equals("/") || filePath.equals("/..")) {
+            status = Status.BAD_REQUEST.toString();
+        } else {
+            Path path = Paths.get(pathToMainDirectory + filePath);
+            try {
+                Files.createDirectories(path.getParent());
+                if(!Files.isWritable(path.getParent())){
+                    return requestSpecification + httpVersion + " " + Status.FORBIDDEN.toString() + "\r\n" + headers + "\r\n";
+                }
+                contentType = Files.probeContentType(path);
+                if (Files.notExists(path)) {
+                    status = Status.CREATED.toString();
+                }
+                Files.write(path, data.getBytes(), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+            } catch (IOException e) {
+                status = Status.BAD_REQUEST.toString();
+            }
+        }
+        return requestSpecification + httpVersion + " " + status + "\r\n" + headers + "Content-Length: " + data.length() + "\r\nContent-Type: " + contentType + "\r\n\r\n" + data;
+    }
+
+    public static String getResponse() {
+        String body = "Home Directory";
+        String contentType = "text/html";
+        String contentDisposition = "inline";
+        if (!filePath.equals("/") && !filePath.equals("/..")) {
+            Path path = Paths.get(pathToMainDirectory + filePath);
+            try {
+                if(Files.notExists(path) || Files.isDirectory(path)) throw new IOException();
+                if(!Files.isReadable(path)){
+                    return requestSpecification + httpVersion + " " + Status.FORBIDDEN.toString() + "\r\n" + headers + "\r\n";
+                }
+                contentType = Files.probeContentType(path);
+                if (!contentType.equals("text/html") && !contentType.equals("text/plain")) {
+                    contentDisposition = "attachment";
+                }
+                body = new String(Files.readAllBytes(path), StandardCharsets.UTF_8);
+            } catch (IOException e) {
+                return requestSpecification + httpVersion + " " + Status.NOT_FOUND.toString() + "\r\n" + headers + "Content-Length: " + body.length() + "\r\nContent-Type: " + contentType + "\r\n\r\n" + "404 Not Found.";
+            }
+        }
+        return requestSpecification + httpVersion + " " + Status.OK.toString() + "\r\n" + headers + "Content-Length: " + body.length() + "\r\nContent-Type: " + contentType + "\r\nContent-Disposition: " + contentDisposition + "\r\n\r\n" + body;
     }
 
     private Packet makeResponsePacket(int responseType, String payload, Packet packet) {
@@ -104,5 +247,14 @@ public class UDPServer {
         int port = Integer.parseInt((String) opts.valueOf("port"));
         UDPServer server = new UDPServer();
         server.listenAndServe(port);
+    }
+
+    private static void resetVars() {
+        httpVersion = "";
+        filePath = "";
+        requestSpecification = "";
+        headers = "";
+        data = "";
+        requestType = null;
     }
 }
